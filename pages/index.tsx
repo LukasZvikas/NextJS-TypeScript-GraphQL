7aee5546ecfa2b4255e0c9f228358360aefd27b0
LukasZvikas <lzvikas1@gmail.com>
2019-05-19 17:23:37 -0400

    added typescript eslint config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,40 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import { NextPage } from 'next';
+import Link from "next/link";
+import * as React from "react";
+import Layout from "../components/Layout";
+import { ApolloConsumer } from 'react-apollo';
+import gql from "graphql-tag";
 
-const IndexPage: NextPage = () => {
-  return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
-      <p><Link href='/about'><a>About</a></Link></p>
-    </Layout>
-  )
-}
+const IndexPage: React.FunctionComponent = () => {
+    return (
+        <Layout title="Home | Next.js + TypeScript Example">
+            <h1>hello Next.js 👋</h1>
+            <p>
+                <Link href="/about">
+                    <a>About</a>
+                </Link>
+            </p>
+            <ApolloConsumer>
+                {(client: any) => (
+                    <button
+                        onClick={async () => {
+                            const response = await client.query({query: gql`
+                query {
+                  loginUser(email: "lzvikas1@gmail.com", password: "1223") {
+                    token
+                    userId
+                    email
+                  }
+                }`
+                            });
+                            console.log('res', response)
+                        }}
+                    >
+            call login mutation
+                    </button>
+                )}
+            </ApolloConsumer>
+        </Layout>
+    );
+};
 
 export default IndexPage;
